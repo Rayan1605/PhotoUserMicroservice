8cfd76e79e3a971faa7fe2fc64442a7dbabefd12
rayan <rayanzyad99@gmail.com>
2024-02-03 23:22:17 +0000

    Start

--- src/main/java/com/example/photousermicroservice/PhotoUserMicroserviceApplication.java
+++ src/main/java/com/example/photousermicroservice/PhotoUserMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.example.photousermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // This will make it a client and communicate with the discovery service
 public class PhotoUserMicroserviceApplication {
 
     public static void main(String[] args) {
