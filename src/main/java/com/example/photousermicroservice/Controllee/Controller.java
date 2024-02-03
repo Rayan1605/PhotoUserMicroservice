848717229f36c96c0cc85f7f0fccdd6f776453b1
rayan <rayanzyad99@gmail.com>
2024-02-03 23:36:27 +0000

    Start

--- src/main/java/com/example/photousermicroservice/Controllee/Controller.java
+++ src/main/java/com/example/photousermicroservice/Controllee/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class Controller {
+
     @GetMapping("/status/check")
     public String status(){
         return "Working";
