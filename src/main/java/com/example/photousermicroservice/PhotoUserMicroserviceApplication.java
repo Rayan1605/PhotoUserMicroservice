8c0e7dfc87506e20d4baa2d0f68c41e8857b09a5
rayan <rayanzyad99@gmail.com>
2024-02-03 23:21:04 +0000

    Start

---
+++ src/main/java/com/example/photousermicroservice/PhotoUserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.photousermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoUserMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoUserMicroserviceApplication.class, args);
+    }
+
+}
